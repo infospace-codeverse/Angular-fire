c44f5989c9b755ce30bda1734e390f7367cfe850
joe052 <joekaranjasenior52@gmail.com>
2024-08-02 22:21:33 +0300

    message

--- task-manager/src/app/components/task-list/task-list.component.ts
+++ task-manager/src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService, Task } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
   imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  removeTask(id: string) {
+    this.taskService.removeTask(id);
+  }
 }
