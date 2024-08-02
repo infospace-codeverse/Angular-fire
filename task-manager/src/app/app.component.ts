1c72c56e634af6d554149163ca95f4ac40666eeb
joe052 <joekaranjasenior52@gmail.com>
2024-08-02 22:10:31 +0300

    message

---
+++ task-manager/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { WrapperComponent } from './wrapper/wrapper.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, WrapperComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'task-manager';
+}
