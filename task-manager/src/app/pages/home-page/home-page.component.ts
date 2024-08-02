749d746e4bfae5492427bce2612375918921302c
joe052 <joekaranjasenior52@gmail.com>
2024-08-02 23:26:28 +0300

    message

--- task-manager/src/app/pages/home-page/home-page.component.ts
+++ task-manager/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { TaskFormComponent } from '../../components/task-form/task-form.component';
-import { TaskListComponent } from '../../components/task-list/task-list.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [TaskFormComponent, TaskListComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
