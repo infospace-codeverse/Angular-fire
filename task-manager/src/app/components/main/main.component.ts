b957a0df9e1048e33c6dbebcb2c79a231b75436c
joe052 <joekaranjasenior52@gmail.com>
2024-08-03 00:51:39 +0300

    message

--- task-manager/src/app/components/main/main.component.ts
+++ task-manager/src/app/components/main/main.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TodoComponent } from '../todo/todo.component';
+import { CommonModule } from '@angular/common';
+import { TodosService } from '../../services/todos.service';
+import { forkJoin } from 'rxjs';
+import { FilterEnum } from '../../types/filter.enum';
+import { TodosFirebaseService } from '../../services/todosFirebase.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TodoComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  todosService = inject(TodosService);
+  todosFirebaseService = inject(TodosFirebaseService);
+  editingId: string | null = null;
 
+  visibleTodos = computed(() => {
+    const todos = this.todosService.todosSig();
+    const filter = this.todosService.filterSig();
+
+    if (filter === FilterEnum.active) {
+      return todos.filter((todo) => !todo.isCompleted);
+    } else if (filter === FilterEnum.completed) {
+      return todos.filter((todo) => todo.isCompleted);
+    }
+    return todos;
+  });
+  isAllTodosSelected = computed(() =>
+    this.todosService.todosSig().every((todo) => todo.isCompleted)
+  );
+  noTodosClass = computed(() => this.todosService.todosSig().length === 0);
+
+  setEditingId(editingId: string | null): void {
+    this.editingId = editingId;
+  }
+
+  toggleAllTodos(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const requests$ = this.todosService.todosSig().map((todo) => {
+      return this.todosFirebaseService.updateTodo(todo.id, {
+        text: todo.text,
+        isCompleted: target.checked,
+      });
+    });
+    forkJoin(requests$).subscribe(() => {
+      this.todosService.toggleAll(target.checked);
+    });
+  }
 }
--- task-manager/src/app/components/todo/todo.component.ts
+++ task-manager/src/app/components/todo/todo.component.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { TodoInterface } from '../../types/todo.interface';
+import { CommonModule } from '@angular/common';
+import { TodosService } from '../../services/todos.service';
+import { TodosFirebaseService } from '../../services/todosFirebase.service';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'app-todos-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
+  @Input({ required: true }) todo!: TodoInterface;
+  @Input({ required: true }) isEditing!: boolean;
+  @Output() setEditingId: EventEmitter<string | null> = new EventEmitter();
 
+  @ViewChild('textInput') textInput?: ElementRef;
+
+  todosService = inject(TodosService);
+  todosFirebaseService = inject(TodosFirebaseService);
+  editingText: string = '';
+
+  ngOnInit(): void {
+    this.editingText = this.todo.text;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isEditing'].currentValue) {
+      setTimeout(() => {
+        this.textInput?.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  changeText(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.editingText = value;
+  }
+
+  changeTodo(): void {
+    const dataToUpdate = {
+      text: this.editingText,
+      isCompleted: this.todo.isCompleted,
+    };
+
+    this.todosFirebaseService
+      .updateTodo(this.todo.id, dataToUpdate)
+      .subscribe(() => {
+        this.todosService.changeTodo(this.todo.id, this.editingText);
+      });
+
+    this.setEditingId.emit(null);
+  }
+
+  setTodoInEditMode(): void {
+    this.setEditingId.emit(this.todo.id);
+  }
+
+  removeTodo(): void {
+    this.todosFirebaseService.removeTodo(this.todo.id).subscribe(() => {
+      this.todosService.removeTodo(this.todo.id);
+    });
+  }
+
+  toggleTodo(): void {
+    const dataToUpdate = {
+      text: this.todo.text,
+      isCompleted: !this.todo.isCompleted,
+    };
+    this.todosFirebaseService
+      .updateTodo(this.todo.id, dataToUpdate)
+      .subscribe(() => {
+        this.todosService.toggleTodo(this.todo.id);
+      });
+  }
 }
